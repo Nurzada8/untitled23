ef94dbaffeb6568a784d7e699cf60307a457b8d1
nurzada <nurzadabakytkyzy8@gmail.com>
2022-05-30 20:05:22 -0700

    HomeWork19

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        DAYS days = DAYS.MONDAY;
+        System.out.println("I will study Java on Monday");
+        DAYS days1 = DAYS.TUESDAY;
+        System.out.println("I study English on Tuesday");
+
+    }
+}
